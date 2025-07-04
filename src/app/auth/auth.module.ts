721df8364c6a0b2be6c91bf4539a1ed30bc9626d
Kang <23302574+KangHidro@users.noreply.github.com>
2025-07-04 10:14:41 +0700

    add login form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
-import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AuthRoutingModule,
     SharedModule,
   ],
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -16,7 +17,10 @@ export class LoginComponent implements OnInit {
   isLoginUser = false;
   isLoginAdmin = false;
 
+  formLogin: FormGroup;
+
   constructor(
+    private fbd: FormBuilder,
     private authSvc: AuthenticateService,
     private socialAuth: SocialAuthService,
     private router: Router,
@@ -32,6 +36,10 @@ export class LoginComponent implements OnInit {
         this.router.navigateByUrl(UrlConstant.ROUTE.MAIN.MY_ANSWER);
       }
     }
+    this.formLogin = this.fbd.group({
+      username: [''],
+      password: ['']
+    });
   }
 
   // login with google
@@ -58,6 +66,27 @@ export class LoginComponent implements OnInit {
             }
           );
         }
+      }
+    );
+  }
+
+  // login with username and password
+  onLogin() {
+    this.spinner.show();
+    this.authSvc.doLoginUserForm(this.formLogin.value).subscribe(
+      (res) => {
+        this.spinner.hide();
+        this.authSvc.setTokenUser(this.formLogin.value.username);
+        this.authSvc.setAuthData(res);
+        if (this.authSvc.checkRoleAdmin()) {
+          this.router.navigateByUrl(UrlConstant.ROUTE.MANAGEMENT.TRAC_NGHIEM);
+        } else if (this.authSvc.checkRoleUser()) {
+          this.router.navigateByUrl(UrlConstant.ROUTE.MAIN.MY_ANSWER);
+        }
+      },
+      (err) => {
+        this.spinner.hide();
+        this.handlerErrorSvc.handleError(err);
       }
     );
   }
